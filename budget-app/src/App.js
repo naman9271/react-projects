4f21a57de33ee3c432df06c9affc8585bc348d15
naman9271 <namanjain9271@gmail.com>
2025-01-16 04:14:12 +0530

    #

--- budget-app/src/App.js
+++ budget-app/src/App.js
@@ -28,7 +28,7 @@ function App() {
     <>
       <Container className="my-4">
         <Stack direction='horizontal' className="mb4" gap="1">
-          <h1 className="me-auto">Budgets</h1>
+          <h1 className="me-auto">BudgetEase</h1>
           <Button variant='primary' onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
           <Button variant='outline-primary' onClick={openAddExpenseModal}>Add Expense</Button>
         </Stack>
